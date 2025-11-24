bc1644a1ded3590e439a1b731b445ea2910e18cf
santiago avila quintero <saq>
2025-11-24 07:57:44 -0500

    feat:se crearon las funciones de mostrar2 y cambiar en el archivo AdminDatos.py junto a sus comentarios y se agrego la funcion a main, modificaciones visulaes menores en el archivo personas.py

--- AdminDatos.py
+++ AdminDatos.py
@@ -3,6 +3,7 @@ from typing import List
 class AdministrarDatos: #clase creada para administrar datos
     def __init__(self):
         self.listaPersonas:List[Persona]=[] #se crea una lista nueva para almacenar los objetos de tipo Persona
+    
     def addper(self,persona: Persona): #se usa un append para añadir los objetos a la lista
         self.listaPersonas.append(persona)
     def mostrar(self): #se ejectura un ciclo para imprimir los datos de cada persona
@@ -14,6 +15,15 @@ class AdministrarDatos: #clase creada para administrar datos
                 c=c+1
                 print("Persona #",c)
                 lista.mostrardatos() #se cita el objeto y se ejecuta la función mostrardatos de la anterior clase
+    def mostrar2(self): #se ejectura un ciclo para imprimir los datos de cada persona como lista
+        c=0 #contador para indicar el numero de persona
+        if not len(self.listaPersonas):
+            print("No hay personas inscritas.")
+        else:
+            for p in self.listaPersonas: #ciclo para recorrer toda la listaPersonas
+                c=c+1
+                print(c,". Nombre: ",p.nombre," Apellido: ",p.apellido," Identificación:",p.identificacion," Edad: ",p.edad," Correo: ",p.correo," Ocupación: ",p.ocupacion," Genero: ",p.genero)
+    
     def borrar(self,numero): #en base al numero de la persona se borra el elemento de la lista.
         del self.listaPersonas[numero-1] #del borra el elemento de la posicion indicada
         print("Persona Borrada")
@@ -29,14 +39,47 @@ class AdministrarDatos: #clase creada para administrar datos
         return checka
     
     def buscar(self):
-        identificacion=input() 
+        identificacion=input("ingresa el numero de identificacion: ") #numero de identificacion de la perosna a buscar
         veri=0           
-        if self.listaPersonas:
-            for p in self.listaPersonas :
-                if p.identificacion == identificacion :
+        if self.listaPersonas:#verifica que la lista no este vacioa 
+            for p in self.listaPersonas :#recorre la lista
+                if p.identificacion == identificacion :#busca que el numero de identificacion se ale mismo que estamos Buscando
                     veri=1
-                    p.mostrardatos()
+                    p.mostrardatos()#muestra la persona
             if veri==0:
                 print("no se encontro la persona")    
         else:
             print("la lista esta vacia")
+   
+    def cambiar(self):
+        identificacion=input("ingresa el numero de identificacion: ") 
+        veri=0          
+        if self.listaPersonas:#verifica que la lista no este vacia
+            for p in self.listaPersonas :#recorre la lista
+                if p.identificacion == identificacion :
+                    veri=1
+                    #se  piden los nuevos varores
+                    nombre=input("Ingrese el nombre: ")
+                    apellido=input("Ingrese el apellido: ")
+                    identificacion=input("Ingrese el numero de indetificación: ")
+                    identificacion=self.checka(identificacion) #revisa si el numero esta repetido y si es un numero
+                    edad=input("Ingrese la edad: ")
+                    edad=self.esnum(edad) #Revisa si la edad es un numero
+                    correo=input("Ingrese el correo: ")
+                    ocupacion=input("Ingrese la ocupación: ")
+                    genero=input("Ingrese el genero: ") #aca se le piden los datos al cliente
+                    #-----se agregan los nuevos valores----- 
+                    p.nombre=nombre
+                    p.apellido=apellido
+                    p.identificacion=identificacion
+                    p.edad=edad
+                    p.correo=correo
+                    p.ocupacion=ocupacion
+                    p.genero=genero
+                    print("\ncambio de datos")
+                    p.mostrardatos()#muestra a la persona
+            if veri==0:
+                print("no se encontro la persona")    
+        else:
+            print("la lista esta vacia")
+    
--- main.py
+++ main.py
@@ -3,38 +3,38 @@ from AdminDatos import AdministrarDatos #importación de clase Administrar datos
 opc=-1
 proce=AdministrarDatos() #Creacion de objeto proce para manejar definiciones de admindatos
 while opc !=0: #ciclo para el menú
-    print("Bienvenido al menú") 
-    print("1. Agregar Persona \n 2. Modificar Persona \n 3. Eliminar Persona \n 4. Mostrar todas las personas \n 5. Buscar persona \n 0. Salir")
+    print("\nBienvenido al menú") 
+    print(" 1. Agregar Persona \n 2. Modificar Persona \n 3. Eliminar Persona \n 4. Mostrar todas las personas \n 5. Buscar persona \n 0. Salir")
     opc=input("Ingrese una opción: ") #Imprime menú y le pide al cliente que ingrese una opción del menú
     if opc=="1":
         nombre=input("Ingrese el nombre: ")
-        #apellido=input("Ingrese el apellido: ")
+        apellido=input("Ingrese el apellido: ")
         identificacion=input("Ingrese el numero de indetificación: ")
         identificacion=proce.checka(identificacion) #revisa si el numero esta repetido y si es un numero
         edad=input("Ingrese la edad: ")
         edad=proce.esnum(edad) #Revisa si la edad es un numero
-        #correo=input("Ingrese el correo: ")
-        #ocupacion=input("Ingrese la ocupación: ")
-        #genero=input("Ingrese el genero: ") #aca se le piden los datos al cliente
-        perso=Persona(nombre,edad,identificacion) #se crea el objeto perso con los atributos que brindó el cliente
+        correo=input("Ingrese el correo: ")
+        ocupacion=input("Ingrese la ocupación: ")
+        genero=input("Ingrese el genero: ") #aca se le piden los datos al cliente
+        perso=Persona(nombre,apellido,edad,identificacion,correo,ocupacion,genero) #se crea el objeto perso con los atributos que brindó el cliente
         print("Datos de la persona: ")
         perso.mostrardatos() #se usa definición mostrar datos para mostrar los datos que se acabaron de ingresar
         proce.addper(perso) #Se añade el objeto a la lista en AdminDatos
         
-    elif opc=="2":
-        print("testy")
+    elif opc=="2":#para cambiar los datos de una persona de la lista
+        proce.cambiar()
 
     elif opc=="4": #para mostrar los datos de todas las personas
-        proce.mostrar()
+        proce.mostrar2()
     elif opc=="3": #Permite eliminar los datos ingresando el numero de la persona, siendo el numero el orden en el cual salen al mostrar toda la lista
-        proce.mostrar()
+        proce.mostrar2()
         numero=int(input("Ingrese la posicion de la persona que desea eliminar: "))
         proce.borrar(numero)
 
-    elif opc=="5":
+    elif opc=="5":#para buscar a una persona por el numero de documento
         proce.buscar()
-
-    elif opc=="0":
+        
+    elif opc=="0":#para salir del menu
         break
     else:
         print("Opción invalida")
